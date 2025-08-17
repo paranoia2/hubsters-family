1d0bea32a279806fb062d5aae5c704e574c7b2b5
paranoia2 <119355067+paranoia2@users.noreply.github.com>
2025-08-17 14:08:01 +0300

    Update jsondb.js

--- src/utils/jsondb.js
+++ src/utils/jsondb.js
@@ -1,18 +1,44 @@
-// src/utils/jsondb.js
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
+import fs from "fs"
 
-// Створюємо адаптер для JSON
-const adapter = new JSONFile('db.json')
+const DB_FILE = "./db.json"
 
-// Створюємо інстанс Low з початковими даними
-const db = new Low(adapter, { users: [] })
+// Читання БД
+function readDB() {
+  try {
+    if (!fs.existsSync(DB_FILE)) {
+      fs.writeFileSync(DB_FILE, JSON.stringify({ users: [] }, null, 2))
+    }
+    const data = fs.readFileSync(DB_FILE, "utf8")
+    return JSON.parse(data)
+  } catch (err) {
+    console.error("Помилка при читанні db.json:", err)
+    return { users: [] }
+  }
+}
 
-// Завантажуємо дані з файлу (або створюємо новий)
-await db.read()
+// Запис БД
+function writeDB(data) {
+  try {
+    fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2))
+  } catch (err) {
+    console.error("Помилка при записі db.json:", err)
+  }
+}
 
-// Якщо файл порожній → ініціалізуємо дефолтними даними
-db.data ||= { users: [] }
-await db.write()
-
-export default db
+export default {
+  getData: () => readDB(),
+  setData: (newData) => writeDB(newData),
+  addUser: (user) => {
+    const db = readDB()
+    db.users.push(user)
+    writeDB(db)
+  },
+  removeUser: (id) => {
+    const db = readDB()
+    db.users = db.users.filter(u => u.id !== id)
+    writeDB(db)
+  },
+  clearUsers: () => {
+    writeDB({ users: [] })
+  }
+}
