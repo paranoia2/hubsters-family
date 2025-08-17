538cd887fbb432219871425c7907dbc8ba6eadae
paranoia2 <119355067+paranoia2@users.noreply.github.com>
2025-08-17 16:37:03 +0300

    Delete src directory

--- src/events/ready.js
+++
@@ -1,9 +0,0 @@
-import { Events, ActivityType } from 'discord.js';
-export default {
-  name: Events.ClientReady,
-  once: true,
-  execute(client) {
-    console.log(`✅ Запущено як ${client.user.tag}`);
-    client.user.setActivity('/play | Hubsters Family', { type: ActivityType.Listening });
-  }
-};
