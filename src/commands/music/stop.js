ed5b53bb51c160f24bfd3c75ffff1fdc4fb7b10f
paranoia2 <119355067+paranoia2@users.noreply.github.com>
2025-08-17 18:15:48 +0300

    Delete src/commands directory

--- src/commands/music/stop.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js'
-import { stop } from '../../utils/music.js'
-export default {
-  data: new SlashCommandBuilder().setName('stop').setDescription('Зупинити музику'),
-  async execute(interaction) {
-    const ok = stop(interaction.guildId)
-    await interaction.reply(ok ? '⏹ Зупинено та вийшов з каналу.' : 'Немає активного плеєра.')
-  }
-}
